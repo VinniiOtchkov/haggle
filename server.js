25e6722193225c618c01ba7dacbc32328c7da259
vinniiotchkov <vinniiotchkov@gmail.com>
2017-07-17 14:53:27 -0700

    added barebones styling to home page

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ var logger = require('morgan');
 var knex = require('./db/knex');
 
 var index = require('./routes/indexRoutes');
-var todos = require('./routes/todosRoutes');
+var items = require('./routes/itemsRoutes');
+var user = require('./routes/userRoutes');
+
 
 var app = express();
 
@@ -16,11 +18,15 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(cors());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+
 
 app.use('/', index);
-app.use('/todos', todos);
+app.use('/items', items);
+app.use('/user', user);
+
 
 app.listen(port, function() {
 console.log("listening on port: ", port);
