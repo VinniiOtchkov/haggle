97149d986a98395043d89477836987525b5786af
Race Carpenter <racecarpenter@gmail.com>
2017-07-18 13:09:26 -0700

    initial user page created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,25 +3,30 @@ var router = express.Router();
 var knex = require('../db/knex');
 
 
-/* GET USER page. */
-router.get('/:id', function(req, res, next) {
-  knex('')
-
-    .where('', req.params.id)
-    .then(function(users) {
-      res.render('', {
-
-      })
-    });
-});
-
 /* Get new User Page. */
 router.get('/new', function(req, res, next) {
-  var users = {};
-  res.render('', {
+  // var users = {};
+  res.render('user_signup');
+});
 
-  })
-})
+/* GET USER page. */
+router.get('/:id', function(req, res, next) {
+  Promise.all([knex('selling_by_id')
+    .join('users', 'users.id', 'selling_by_id.seller_id')
+    .select('seller_name', 'img_url', 'item_name', 'haggle_price', 'buyer_name', 'status')
+    .where('seller_id', req.params.id),
+    knex('buyer_by_id')
+    .join('users', 'users.id', 'buyer_by_id.buyer_id')
+    .select('img_url', 'item_name', 'haggle_price', 'seller_name', 'city', 'status')
+    .where('buyer_id', req.params.id)
+  ]).then(function(users) {
+    console.log('users', users);
+    res.render('user', {
+      selling: users[0],
+      buying: users[1]
+    });
+  });
+});
 
 /* Deletes User. */
 router.get('/:id/remove', function(req, res, next) {
@@ -31,27 +36,30 @@ router.get('/:id/remove', function(req, res, next) {
     .then(function() {
       res.redirect('/');
     });
-})
+});
 
 /* Creates New User. */
-router.post('/newUser', function(req, res, next) {
-  knex('')
+router.post('/new', function(req, res, next) {
+  knex('users')
     .insert({
-
+      name: '${req.body.name}',
+      email: '${req.body.email}',
+      location_id: '${req.body.location_id}'
     })
-    .then(function() {
-
+    .then(function(req, res) {
+      res.redirect('/');
     });
 });
+
 
 /* Updates User */
 router.post('/:id/update', function(req, res) {
   knex('')
     .update(req.body)
-    .where('', req.params.id)
+    .where('id', req.params.id)
     .then(function(users) {
       res.redirect(`/${req.params.id}`)
-    })
+    });
 });
 
 
